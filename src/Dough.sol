4e3c1c87f3a69364f9ddf9a72db5aaf1756be2f1
Hunter King <hking6776@gmail.com>
2024-10-12 20:03:38 +0200

    add swap functionality

--- src/Dough.sol
+++ src/Dough.sol
@@ -5,6 +5,8 @@ import {IERC20} from "@oz/token/ERC20/IERC20.sol";
 import {ICurveStableSwap, ICurveStableSwapV2} from "src/interfaces/ICurveStableSwap.sol";
 
 interface IDough {
+    event RefillBalance(address _safe, uint256 _breadExchanged, uint256 _eureRecieved);
+
     struct SafeSettings {
         uint256 refillTrigger;
         uint256 refillAmount;
@@ -18,8 +20,11 @@ interface IDough {
  * 1. add add Gnosis Pay Safe addr and verify it is a Gnosis Pay Safe
  */
 contract Dough is IDough {
+    uint256 constant WAD = 1 ether;
     IERC20 constant GNOSIS_EURE = IERC20(0xcB444e90D8198415266c6a2724b7900fb12FC56E);
     IERC20 constant GNOSIS_BREAD = IERC20(0xa555d5344f6FB6c65da19e403Cb4c1eC4a1a5Ee3);
+    ICurveStableSwap constant CRV_WXDAI_BREAD = ICurveStableSwap(0xf3D8F3dE71657D342db60dd714c8a2aE37Eac6B4);
+    ICurveStableSwap constant CRV_WXDAI_USDC_USDT = ICurveStableSwap(0x7f90122BF0700F9E7e1F688fe926940E8839F353);
     ICurveStableSwapV2 constant CRV_EURE_USD = ICurveStableSwapV2(0x056C6C5e684CeC248635eD86033378Cc444459B0);
 
     uint256 internal _counter; // set limit for address(this) to limit array-based DOS (out of gas) attack
@@ -28,14 +33,8 @@ contract Dough is IDough {
 
     address[] internal _safeRegistry;
 
-    // function resolveRefillBalances(address[] memory _safes) external view {
-    //     uint256 l = _safes.length;
-    //     for (uint256 i = 0; i < l; i++) {
-    //         resolveRefillBalance(_safes[i]);
-    //     }
-    // }
-
     function addSafeToRegistry() external {
+        // confirm Safe is associated with Gnosis Pay, add Safe to data structures, add max approvals
         _counter += 1;
     }
 
@@ -48,13 +47,11 @@ contract Dough is IDough {
             SafeSettings memory ss = _safeSettings[_safe];
 
             if (GNOSIS_EURE.balanceOf(_safe) < ss.refillTrigger) {
-                // calculate exchange rate
-                uint256 _priceUsdToEure = CRV_EURE_USD.price_oracle();
-                uint256 _refillAmount = (ss.refillAmount - GNOSIS_EURE.balanceOf(_safe)) * _priceUsdToEure / 1 ether;
+                uint256 _amount = (ss.refillAmount - GNOSIS_EURE.balanceOf(_safe)) * WAD / CRV_EURE_USD.price_oracle();
 
-                if (GNOSIS_BREAD.balanceOf(_safe) > _refillAmount) {
+                if (GNOSIS_BREAD.balanceOf(_safe) > _amount) {
                     _safes[i] = _safe;
-                    _amounts[i] = _refillAmount;
+                    _amounts[i] = _amount;
                 }
             }
         }
@@ -72,7 +69,19 @@ contract Dough is IDough {
 
         uint256 l = _safes.length;
         for (uint256 i; i < l; i++) {
-            // do the swap
+            address _safe = _safes[i];
+            uint256 _amount = _amounts[i];
+
+            GNOSIS_BREAD.transferFrom(_safe, address(this), _amount);
+            uint256 _wxdai = CRV_WXDAI_BREAD.exchange(0, 1, _amount, _amount * 0.97 ether / WAD); // minimum 97% exchange rate
+
+            // TODO: make calldata?
+            uint256[] memory _lp3usd = new uint256[](3);
+            _lp3usd[0] = _wxdai;
+            uint256 _3usd = CRV_WXDAI_USDC_USDT.add_liquidity(_lp3usd, _wxdai * 0.97 ether / WAD);
+            uint256 _eure = CRV_EURE_USD.exchange(1, 0, _3usd, _3usd * CRV_EURE_USD.price_oracle() / WAD, _safe);
+
+            emit RefillBalance(_safe, _amount, _eure);
         }
     }
 }
